02a59a9409a72dd923f9d6bf3938dd624b7c8f1d
avielb <aviel33@gmail.com>
2019-04-26 17:33:17 +0300

    fix commands, add get_info.sh, add values.yaml

---
+++ helm/get_info.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+base_url="http://"$(minikube ip)":"$(kubectl get svc myblog-wordpress -o=jsonpath='{.spec.ports[?(@.port==80)].nodePort}')
+echo "WordPress URL is: $base_url"
+echo "Management Console: ${base_url}/wp-login.php"
+echo -e "Username: user\nPassword: "$(kubectl get secret --namespace default myblog-wordpress -o jsonpath="{.data.wordpress-password}" | base64 --decode) # to extract credentials
+
