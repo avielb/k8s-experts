5c083cf302b1bc079945b9f1ecec06007b54b2d2
avielb <aviel33@gmail.com>
2020-01-25 14:26:48 +0200

    add provision.sh and some more cosmetics

---
+++ on-premise/rancher/provision.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce -y
+systemctl start docker
+systemctl enable docker
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
