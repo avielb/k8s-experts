458436c299fc5d291dac0d60d88c869788c921bf
avielb <aviel33@gmail.com>
2020-07-22 07:50:32 +0300

    fixed version for rancher

--- on-premise/rancher/provision_rancher.sh
+++ on-premise/rancher/provision_rancher.sh
@@ -3,4 +3,4 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install docker-ce -y
 systemctl start docker
 systemctl enable docker
-docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher:v2.4.5
